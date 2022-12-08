b6929ec41b1b7da9cc76cbf1021c6d7050d4f974
maya koma <mayakoma@gmail.com>
2022-12-08 23:21:47 +0200

    server and socket

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -11,6 +11,9 @@ wss.on("connection", (ws) => {
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const HttpError = require("./model/httpError");
+const codeRoute = require("./routes/code-route");
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,3 +27,31 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
+
+app.use("/code", codeRoute);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could  not find this route", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://mayakoma:mayakoma7@cluster0.wkzoen0.mongodb.net/code?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    const server = app.listen(5000, () => console.log("listen  port 5000"));
+    const io = require("socket.io")(server);
+    io.on("connection", (socket) => {
+      console.log("c c");
+    });
+  })
+  .catch((err) => console.log(err));
